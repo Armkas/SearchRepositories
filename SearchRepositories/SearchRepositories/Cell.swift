446c10f58cb28263384c019f72763392ceac6b5d
puyue <pu.yue@access-company.com>
2021-12-02 01:40:37 +0900

    Build UI

---
+++ SearchRepositories/SearchRepositories/Cell.swift
@@ -0,0 +1,18 @@
+//
+//  Cell.swift
+//  SearchRepositories
+//
+//  Created by user on 2021/12/02.
+//
+
+import Foundation
+import UIKit
+
+class Cell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+}
--- SearchRepositories/SearchRepositories/ViewController.swift
+++ SearchRepositories/SearchRepositories/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupTableView()
+        textField.addTarget(self, action: #selector(self.textDidChange(_:)), for: .editingChanged)
+    }
+    
+    private func setupTableView() {
+        tableView.register(UINib(nibName: "Cell", bundle: .main), forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    @objc private func textDidChange(_ sender: UITextField) {
+       print("!@#1")
+        //这里设定显示小菊花,小菊花消失了才访问服务器,免得重复访问
+        getData()
+    }
+    
+    func getData() {
+        
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
 
 }
 
