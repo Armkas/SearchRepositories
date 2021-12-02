b4624b7454fcf63e0def462f539adbe0a0d87f06
puyue <pu.yue@access-company.com>
2021-12-03 01:28:35 +0900

    Change UI

---
+++ SearchRepositories/SearchRepositories/API/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  SearchRepositories
+//
+//  Created by user on 2021/12/02.
+//
+
+import Foundation
+
+final class API {
+    
+}
--- SearchRepositories/SearchRepositories/UI/ViewController.swift
+++ SearchRepositories/SearchRepositories/UI/ViewController.swift
@@ -10,31 +10,41 @@ import Foundation
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    var timer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchBar.delegate = self
         setupTableView()
-        textField.addTarget(self, action: #selector(self.textDidChange(_:)), for: .editingChanged)
     }
     
     private func setupTableView() {
         tableView.register(UINib(nibName: "Cell", bundle: .main), forCellReuseIdentifier: "Cell")
         tableView.dataSource = self
-        tableView.delegate = self
     }
     
-    @objc private func textDidChange(_ sender: UITextField) {
-       print("!@#1")
-        //这里设定显示小菊花,小菊花消失了才访问服务器,免得重复访问
-        getData()
+    @objc func getRepositories(_ searchBar: UISearchBar) {
+        print("!@#$")
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(
+            timeInterval: 0.5,
+            target: self,
+            selector: #selector(self.getRepositories(_:)),
+            userInfo: nil,
+            repeats: false
+        )
     }
     
-    func getData() {
-        
-    }
 }
 
 extension ViewController: UITableViewDataSource {
@@ -45,11 +55,4 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
     }
-
-
 }
-
-extension ViewController: UITableViewDelegate {
-
-}
-
